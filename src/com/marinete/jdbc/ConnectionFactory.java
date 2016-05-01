a32b3c89f63ef2195cfe03768c26e432b563fce3
Gabs <g4brielfernandes@gmail.com>
2016-05-01 19:21:24 -0300

    postgres

--- src/com/marinete/jdbc/ConnectionFactory.java
+++ src/com/marinete/jdbc/ConnectionFactory.java
@@ -9,8 +9,10 @@ public class ConnectionFactory {
 	public static Connection getConnection() throws ClassNotFoundException {
 
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			return DriverManager.getConnection("jdbc:mysql://localhost:3306/marinete?autoReconnect=true&useSSL=false", "root", "nathan07daredevilgoteyes");
+			//Class.forName("com.mysql.jdbc.Driver");
+			String dbUrl = System.getenv("JDBC_DATABASE_URL");
+			return DriverManager.getConnection(dbUrl);
+			//return DriverManager.getConnection("jdbc:mysql://localhost:3306/marinete?autoReconnect=true&useSSL=false", "root", "nathan07daredevilgoteyes");
 
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
