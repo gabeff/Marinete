143edef925a55449d3308a84ce2d432ca9da80eb
Gabs <g4brielfernandes@gmail.com>
2016-05-22 00:53:37 -0300

    merge desenv listar marinetes jsp

--- src/br/com/marineteapp/jdbc/ConnectionFactory.java
+++ src/br/com/marineteapp/jdbc/ConnectionFactory.java
@@ -9,10 +9,10 @@ public class ConnectionFactory {
 	public static Connection getConnection() throws ClassNotFoundException {
 
 		try {
-//			String dbUrl = System.getenv("JDBC_DATABASE_URL");
-//			return DriverManager.getConnection(dbUrl);
-			Class.forName("com.mysql.jdbc.Driver");
-			return DriverManager.getConnection("jdbc:mysql://localhost:3306/marinete?autoReconnect=true&useSSL=false", "root", "root");
+			String dbUrl = System.getenv("JDBC_DATABASE_URL");
+			return DriverManager.getConnection(dbUrl);
+//			Class.forName("com.mysql.jdbc.Driver");
+//			return DriverManager.getConnection("jdbc:mysql://localhost:3306/marinete?autoReconnect=true&useSSL=false", "root", "root");
 
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
